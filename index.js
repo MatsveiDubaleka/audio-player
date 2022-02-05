78b09eff879c6e45c1012a98c9f3de1c98c3caf5
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-02-05 18:56:57 +0300

    feat: add functions playSong and pauseSong, and listerner

--- index.js
+++ index.js
@@ -18,8 +18,32 @@ let songIndex = 0;
 // Init
 function loadSong(song) {
     title.innerHTML = song;
-    audio.src = `assets/${song}.np3`
-    cover.src = `assets/img/cover${songIndex + 1}.png`
+    audio.src = `./assets/audio/${song}.mp3`
+    cover.src = `./assets/img/cover${songIndex + 1}.png`
 }
 
 loadSong(songs[songIndex])
+
+// Play
+function playSong () {
+    player.classList.add('play')
+    audio.play()
+    audio.currentTime = 0;
+    imgSrc.src = './assets/svg/pause.png'
+}
+
+// Pause
+function pauseSong () {
+    player.classList.remove('play')
+    audio.pause()
+    imgSrc.src = './assets/svg/play.png'
+}
+
+playBtn.addEventListener('click', () => {
+    const isPlay = player.classList.contains('play')
+    if(isPlay) {
+        pauseSong()
+    } else {
+        playSong()
+    }
+})
