5522bf94699631737c218aded7e4e58ba5de5342
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-02-05 18:37:29 +0300

    feat: add function Load Song

--- index.js
+++ index.js
@@ -5,7 +5,21 @@ const player = document.querySelector('.player'),
       audio = document.querySelector('.audio'), 
       progressContainer = document.querySelector('.progress__container'), 
       progress = document.querySelector('.progress'),
-      title = document.querySelector('.title'),
-      coverImg = document.querySelector('.cover__img'),
+      title = document.querySelector('.song'),
+      cover = document.querySelector('.cover__img'),
       imgSrc = document.querySelector('.img__src')
 
+// Songs
+const songs = ['beyonce', 'dontstartnow']
+
+// Песня по умолчанию
+let songIndex = 0;
+
+// Init
+function loadSong(song) {
+    title.innerHTML = song;
+    audio.src = `assets/${song}.np3`
+    cover.src = `assets/img/cover${songIndex + 1}.png`
+}
+
+loadSong(songs[songIndex])
