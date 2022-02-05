4f66516a63f5886ac8cfc86ac75aece8adde1b31
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-02-05 18:31:06 +0300

    feat: add DOM elements

---
+++ index.js
@@ -0,0 +1,11 @@
+const player = document.querySelector('.player'),
+      playBtn = document.querySelector('.play'),
+      prevBtn = document.querySelector('.prev'),
+      nextBtn = document.querySelector('.next'),
+      audio = document.querySelector('.audio'), 
+      progressContainer = document.querySelector('.progress__container'), 
+      progress = document.querySelector('.progress'),
+      title = document.querySelector('.title'),
+      coverImg = document.querySelector('.cover__img'),
+      imgSrc = document.querySelector('.img__src')
+
